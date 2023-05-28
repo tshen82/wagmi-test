91e50775088a62b7dd0a8c1ba92bfd3ff62ccd30
Glitch <glitch.txs@gmail.com>
2023-05-28 00:37:00 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,44 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createConfig, configureChains, mainnet } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { WagmiConfig } from 'wagmi'
+
+const { chains, publicClient } = configureChains([mainnet], [publicProvider()])
+ 
+const config = createConfig({
+  connectors: [
+    new InjectedConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi.sh',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+        metadata: {
+          name: 'wagmi',
+          description: 'my wagmi app',
+          url: 'https://wagmi.sh',
+          icons: ['https://wagmi.sh/icon.png'],
+        },
+      },
+    })
+    
+  ],
+  publicClient,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={config} >
+      <Component {...pageProps} />
+    </WagmiConfig>
+    )
 }
